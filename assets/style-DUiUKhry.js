b39884d8c46871ed878f677a0c04d88d8cff7b1d
Young-abc <3219337115@qq.com>
2026-05-13 19:34:58 +0800

    Delete assets directory

--- assets/style-DUiUKhry.js
+++
@@ -1,1 +0,0 @@
-import"./index-DaSCmevB.js";/* empty css             */
--- assets/user-CVcDpo6u.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-DaSCmevB.js";var t=e(`user`,{state:()=>({user:{}}),actions:{setUserInfo(e){this.user=e},updateUserInfo(e){this.user=e.userInfo}},persist:!0});export{t};
